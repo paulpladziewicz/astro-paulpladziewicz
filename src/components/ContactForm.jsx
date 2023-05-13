a8c48b81158417a86330a1dc4106d4d324745064
paulpladziewicz <ppladziewicz@gmail.com>
2023-05-13 06:28:10 -0400

    completed project card format

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -9,13 +9,34 @@ const translation = {
     }
 }
 
-const ContactForm = ({lang = 'en'}) => {
+const ContactForm = ({lang}) => {
+    lang = lang || 'en';
     const { title } = translation[lang];
 
     return (
         <section className="row">
-            {title}
-            <button>Click</button>
+            <h2 className="display-3 fw-bold text-center mb-4">Reach Out</h2>
+            <form className="col-md-6 mx-auto">
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Full Name</label>
+                    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Phone Number</label>
+                    <input type="tel" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <div id="emailHelp" className="form-text">Your phone number will remain private.</div>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <div id="emailHelp" className="form-text">Your email address will remain private.</div>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Message</label>
+                    <textarea className="form-control" id="exampleInputPassword1" />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
         </section>
     )
 }
